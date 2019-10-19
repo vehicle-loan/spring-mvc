769afc8c8bffbd9fc00f9b2cfb62b9376ca23736
Helpu <helperbhai700@gmail.com>
2019-10-19 17:59:12 +0530

    Commit at the end of day(19-10-2019)

--- vehicle-loan-web/src/main/java/com/lti/controller/RegisterController.java
+++ vehicle-loan-web/src/main/java/com/lti/controller/RegisterController.java
@@ -8,8 +8,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.lti.vehicleloan.dto.RegisterDataDTO;
 import com.lti.vehicleloan.entity.UserPersonalDetails;
 import com.lti.vehicleloan.service.RegisterService;
+import com.lti.vehicleloan.entity.UserCredentials;
 
 @Controller
 public class RegisterController {
@@ -18,10 +20,19 @@ public class RegisterController {
 	private RegisterService registerService;
 	
 	@RequestMapping(path="/register.lti",method=RequestMethod.POST)
-	public String register (UserPersonalDetails userPersonalDetails,Map Model) {
-		System.out.println(userPersonalDetails.getName());
-		registerService.register(userPersonalDetails);
-		return "success.jsp";
+	public String register (RegisterDataDTO data,Map Model) {
+		
+		UserPersonalDetails upd = new UserPersonalDetails();
+		upd.setName(data.getName());
+		upd.setGender(data.getGender());
+		upd.setMobileNumber(data.getMobileNumber());
+		
+		UserCredentials uc = new  UserCredentials();
+		uc.setEmail(data.getEmail());
+		uc.setPassword(data.getPassword());
+		upd.setUserCredentials(uc);
+		registerService.register(upd);
+		return "GetEligibiltyCriteria.jsp";
 	}
 	
 }
